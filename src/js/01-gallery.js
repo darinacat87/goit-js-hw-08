14405ba4d32e0dcd587904552c3cd66b9f8e3202
Daria Sliusar <darinacat87@gmail.com>
2022-09-04 21:53:22 +0300

    Update 02-video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const listGalleryEl = galleryItems
+  .map(
+    image =>
+      `<a class="gallery__item" href="${image.original}">
+            <img 
+                class="gallery__image"
+                src="${image.preview}"
+                alt="${image.description}"
+            />
+        </a>
+    `
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', listGalleryEl);
+
+let lightBox = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
+
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const throttle = require('lodash.throttle');
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(function () {
+    player.getCurrentTime().then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    });
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
