1e6275b9c6e56bb097247faaf52a7c3496f240ec
Daria Sliusar <darinacat87@gmail.com>
2022-09-05 22:40:10 +0300

    Update 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+localStorage.setItem(
+  LOCALSTORAGE_KEY,
+  JSON.stringify({ email: '', message: '' })
+);
+
+form.addEventListener('keydown', throttle(saveForm, 500));
+
+const { email, message } = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+form.email.value = email;
+form.message.value = message;
+
+form.addEventListener('submit', submitForm);
+
+function saveForm() {
+  localStorage.setItem(
+    LOCALSTORAGE_KEY,
+    JSON.stringify({ email: form.email.value, message: form.message.value })
+  );
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  console.log({ email: form.email.value, message: form.message.value });
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  form.reset();
+}
